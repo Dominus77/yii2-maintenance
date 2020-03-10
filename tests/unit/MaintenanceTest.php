ae9c988dd513e854ea557613e7bc36670cb25994
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-03-10 15:07:52 +0300

    add tests

--- tests/unit/MaintenanceTest.php
+++ tests/unit/MaintenanceTest.php
@@ -44,4 +44,29 @@ class MaintenanceTest extends Unit
         $this->tester->assertEquals($maintenance->statusCode, Maintenance::STATUS_CODE_MAINTENANCE);
         $this->state->disable();
     }
+
+    public function testBootstrapOk()
+    {
+        $maintenance = new Maintenance($this->state);
+        $maintenance->bootstrap(Yii::$app);
+
+        $headers = Yii::$app->response->getHeaders()->toArray();
+        $this->tester->assertArrayNotHasKey('retry-after', $headers);
+        $this->tester->assertEquals(Yii::$app->response->statusCode, Maintenance::STATUS_CODE_OK);
+    }
+
+    public function testBootstrapMaintenance()
+    {
+        $this->state->enable();
+        $maintenance = new Maintenance($this->state);
+        $maintenance->bootstrap(Yii::$app);
+
+        $headers = Yii::$app->response->getHeaders()->toArray();
+        $this->tester->seeMyVar($headers['retry-after']);
+        $this->tester->assertArrayHasKey('retry-after', $headers);
+
+        $timestamp = $this->state->timestamp();
+        $this->tester->assertEquals($headers['retry-after'][0], gmdate('D, d M Y H:i:s \G\M\T', $timestamp));
+        $this->state->disable();
+    }
 }
